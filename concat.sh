aa2743738425c8baaa3dad7bf87d7fff4ebe79c9
bripod <bripod@gmail.com>
2014-02-07 20:45:53 -0500

    quick fix
    Changed from "*.js" to list; fixed erroneous filenames; create tmp.js
    and rm it so that awk won't complain about overwriting the same file
    it's looking at.

--- concat.sh
+++ concat.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # roll20 Script Concatenator
 
-cat modules/*.js > DCCHelper.js
+# concat all files
+# cat modules/*.js > tmp.js
+
+# concat list of files
+
+cat modules/roll20library.js modules/attrib.js modules/diceChain.js modules/deed.js modules/spellDuel.js modules/clericSpell.js modules/wizardSpell.js > tmp.js
 
 # clean up debug comments
 # i.e. lines in the concatenated file that are of the format 'tmp = ...; log(tmp);'
 # which can also easily be commented on/off via regex
-awk '{gsub(/[[:space:]]+tmp = [^\n]+ log\(tmp\);/,"");print}' dccHelper_test.js > dccHelper_test_out.js
-
+awk '{gsub(/[[:space:]]+tmp = [^\n]+ log\(tmp\);/,"");print}' tmp.js > DCCHelper.js
+rm tmp.js
 
